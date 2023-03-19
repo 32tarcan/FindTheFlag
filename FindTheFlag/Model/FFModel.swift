15782b452c0d74446f5be2a032537e6f04c9cc65
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-19 03:17:36 +0300

    Model folder created and models added.

---
+++ FindTheFlag/Model/FFModel.swift
@@ -0,0 +1,31 @@
+//
+//  Model.swift
+//  FindTheFlag
+//
+//  Created by Bahadır Tarcan on 19.03.2023.
+//
+
+import Foundation
+
+class FFModel: Codable {
+    let country: [String]
+}
+
+let country = [
+    "turkey",
+    "usa",
+    "germany",
+    "france",
+    "italy",
+    "spain",
+    "uk",
+    "china",
+    "japan",
+    "canada",
+    "brazil",
+    "argentina",
+    "india",
+    "russia",
+    "australia",
+    "southAfrica"
+]
