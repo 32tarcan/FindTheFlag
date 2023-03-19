ad75d570183c3cc2ccd5df54cacfb73b90aa8142
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-19 03:17:12 +0300

    Add FFViewModelAPI class with delegate and network functionality

--- FindTheFlag/ViewModels/FFViewModelAPI.swift
+++ FindTheFlag/ViewModels/FFViewModelAPI.swift
@@ -5,7 +5,64 @@
 //  Created by Bahadır Tarcan on 19.03.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol FFViewModelDelegate: AnyObject {
+    func didLoadFlags(flags: [FFModel], country: FFModel)
+    func didFailWithError(error: Error)
+}
+
+class FFViewModelAPI {
+    
+    var delegate: FFViewModelDelegate?
+    
+    let flagAPI = "https://flag.herokuapp.com/country"
+    
+    func fetchFlag() {
+        let urlString = "\(flagAPI)/\(country)"
+        performRequest(with: urlString)
+        
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let flags = self.parseJSON(safeData) {
+                        if let country = flags.first {
+                            self.delegate?.didLoadFlags(flags: flags, country: country)
+                        }
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    
+    
+    func parseJSON(_ data: Data) -> [FFModel]? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode([FFModel].self, from: data)
+            return decodedData
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+        
+    }
+}
+
+
+
+
 
 
 
