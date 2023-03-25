0840d9a650fc25500787870d63dc22e1a9bb229d
32tarcan <bahadir.tarcan41@gmail.com>
2023-03-25 23:13:44 +0300

    Score Label and Constraints added.

--- FindTheFlag/Views/FFView.swift
+++ FindTheFlag/Views/FFView.swift
@@ -9,10 +9,12 @@ import UIKit
 
 
 
+
 class FFView: UIView  {
     let viewModel: FFViewModelAPI
     let viewController = FFViewController()
     var ffmodel: FFModel
+    var score = FFScoreViewController()
     
 
     
@@ -98,10 +100,7 @@ class FFView: UIView  {
             correctAnswerFound = true
         
                 DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
-                    self.countryButton1.backgroundColor = .systemBrown
-                    self.countryButton2.backgroundColor = .systemBrown
-                    self.countryButton3.backgroundColor = .systemBrown
-                    self.countryButton4.backgroundColor = .systemBrown
+                   
                     self.correctAnswer = Int.random(in: 1...4)
                     self.correctAnswerFound = false
                     
@@ -142,6 +141,8 @@ class FFView: UIView  {
         addSubview(countryButton3)
         addSubview(countryButton4)
         addSubview(countryImageView)
+        addSubview(scoreLabel)
+        
         
         
     }
@@ -152,35 +153,46 @@ class FFView: UIView  {
             countryButton1.topAnchor.constraint(equalTo: topAnchor, constant: 500),
             countryButton1.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
             countryButton1.trailingAnchor.constraint(equalTo: countryButton2.leadingAnchor, constant: -20),
-            countryButton1.bottomAnchor.constraint(equalTo: countryButton3.topAnchor, constant: -20), // countryButton1 ve countryButton3 arasında 20 birim yatay boşluk bırakılıyor
+            countryButton1.bottomAnchor.constraint(equalTo: countryButton3.topAnchor, constant: -20),
             countryButton1.widthAnchor.constraint(equalTo: countryButton2.widthAnchor),
             countryButton1.heightAnchor.constraint(equalTo: countryButton3.heightAnchor),
 
             // Button 2
             countryButton2.topAnchor.constraint(equalTo: topAnchor, constant: 500),
-            countryButton2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30), // countryButton2'ye 50 piksel soldan boşluk bırakılıyor
-            countryButton2.bottomAnchor.constraint(equalTo: countryButton4.topAnchor, constant: -20), // countryButton2 ve countryButton4 arasında 20 birim dikey boşluk bırakılıyor
+            countryButton2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+            countryButton2.bottomAnchor.constraint(equalTo: countryButton4.topAnchor, constant: -20),
             countryButton2.widthAnchor.constraint(equalTo: countryButton1.widthAnchor),
             countryButton2.heightAnchor.constraint(equalTo: countryButton4.heightAnchor),
 
             // Button 3
             countryButton3.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
             countryButton3.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100),
-            countryButton3.trailingAnchor.constraint(equalTo: countryButton4.leadingAnchor, constant: -20), // countryButton3 ve countryButton4 arasında 20 birim yatay boşluk bırakılıyor
+            countryButton3.trailingAnchor.constraint(equalTo: countryButton4.leadingAnchor, constant: -20),
             countryButton3.widthAnchor.constraint(equalTo: countryButton4.widthAnchor),
             countryButton3.heightAnchor.constraint(equalTo: countryButton1.heightAnchor),
 
             // Button 4
-            countryButton4.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30), // countryButton4'e 50 piksel sağdan boşluk bırakılıyor
+            countryButton4.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
             countryButton4.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100),
             countryButton4.widthAnchor.constraint(equalTo: countryButton3.widthAnchor),
             countryButton4.heightAnchor.constraint(equalTo: countryButton2.heightAnchor),
             
+            // Flag Image
+            countryImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            countryImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -110),
+            countryImageView.widthAnchor.constraint(equalToConstant: 300),
+            countryImageView.heightAnchor.constraint(equalToConstant: 200),
             
-                countryImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-                countryImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -110),
-                countryImageView.widthAnchor.constraint(equalToConstant: 300),
-                countryImageView.heightAnchor.constraint(equalToConstant: 200)
+            //Score label
+            scoreLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            scoreLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20)
+            
+            // Bayrağın üzerinde durmasını istiyorsan sor!
+            //scoreLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            //scoreLabel.bottomAnchor.constraint(equalTo: countryImageView.topAnchor, constant: -30)
+
+           
+            
             ])
             
             
@@ -194,7 +206,15 @@ class FFView: UIView  {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-        
     
+    
+    let scoreLabel: UILabel = {
+        let scoreLabel = UILabel()
+        scoreLabel.text = "Score 10"
+        scoreLabel.textColor = .black
+        scoreLabel.font = UIFont(name: "Chalkduster", size: 19)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        return scoreLabel
+    }()
     
 }
